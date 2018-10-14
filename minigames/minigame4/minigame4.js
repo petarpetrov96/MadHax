021965a23b756377918c18d271973419ae06af73
Matus Mikus <matus.mikus@gmail.com>
2018-10-14 11:25:19 +0100

    instructions for 4th gmae

--- minigames/minigame4/minigame4.js
+++ minigames/minigame4/minigame4.js
@@ -1,75 +1,9 @@
-//add two buttons, inspection and repair
-
 var connections = shuffleArray(new Array(0,1,2,3,4));
 var faulty = getFaultyIndeces();
 var faultyValves = faulty.length;
 var repairRate = [0,0,0,0,0];
 var inspect = false;
 var repair = false;
-
-
-function setUpGraphics(){
-	var main_window = document.getElementById("window");
-	
-	//water
-	var waterBackground = document.createElement("div");
-	waterBackground.setAttribute("id","minigame2_water");
-	waterBackground.style.width = "500px";
-	waterBackground.style.height = "500px";
-	main_window.appendChild(waterBackground);
-	waterStart = [waterBackground.offsetLeft, waterBackground.offsetTop];
-	containerStart = waterStart;
-	waterEnd = [waterStart[0]+waterBackground.offsetWidth,
-				waterStart[1]+waterBackground.offsetHeight];
-	containerEnd = waterEnd;
-	//water container
-	var containerSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	containerSvg.setAttribute("width","500");
-	containerSvg.setAttribute("height","500");
-	containerSvg.setAttribute('id','minigame2_container');
-	var svgNS = containerSvg.namespaceURI;
-	var polyline = document.createElementNS(svgNS,'polyline');
-	polyline.setAttribute('points', 
-								(waterStart[0]-195) + "," + (waterStart[1]-150) + " " +
-								(waterStart[0]-195) + "," + (waterEnd[1]-145)+ " " + 
-								(waterEnd[0]- 205) + "," + (waterEnd[1]-145) + " " + 
-								(waterEnd[0]- 205) + "," + (waterStart[1]-150));
-
-	polyline.setAttribute('style','fill:none;stroke:black;stroke-width:10');
-	containerSvg.appendChild(polyline);
-	main_window.appendChild(containerSvg);
-		
-	//"generator"
-	var genSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	genSvg.setAttribute("width","100");
-	genSvg.setAttribute("height","100");
-	genSvg.setAttribute('id','minigame2_generator');
-	var svgNS = genSvg.namespaceURI;
-	var rect = document.createElementNS(svgNS,'rect');
-	rect.setAttribute('x',0);
-	rect.setAttribute('y',0);
-	rect.setAttribute('width',100);
-	rect.setAttribute('height',100);
-	rect.setAttribute('fill','#000000');
-		genSvg.appendChild(rect);
-	
-
-	//"bucket"
-	svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svg.setAttribute("width","100");
-	svg.setAttribute("height","100");
-	svg.style.position = "absolute";
-	var svgNS = svg.namespaceURI;
-	var rect = document.createElementNS(svgNS,'rect');
-	rect.setAttribute('x',0);
-	rect.setAttribute('y',0);
-	rect.setAttribute('width',100);
-	rect.setAttribute('height',100);
-	rect.setAttribute('fill','#3b3b3b');
-	svg.appendChild(rect);
-	main_window.appendChild(svg);
-	main_window.appendChild(genSvg);
-}
 
 function setUp(){
 	var main_window = document.getElementById("window");
@@ -151,6 +85,15 @@ function setUpSvg(){
 	main_window.appendChild(containerSvg);
 }
 
+function setUpInstructions(){
+	var main_window = document.getElementById("window");
+	var instructions = document.createElement("p");
+	instructions.innerHTML = "Instructions: select the inspection tool to inspect the valves on the left. " + 
+							"Inspecting takes 2 seconds. Once you find the broken valves, select the repair tool to repair them. " + 
+							"Repairing takes 5 clicks. Repair all the valves to win!";
+	main_window.appendChild(instructions);
+}
+
 /**
  * Randomize array element order in-place.
  * Using Durstenfeld shuffle algorithm.
@@ -194,7 +137,6 @@ function leftValveClick(index){
 
 function getFaultyIndeces(){
 	var noOfFaulty = Math.floor(Math.random() * 3)+1;
-	//console.log(noOfFaulty);
 	var shuffled = shuffleArray(new Array(0,1,2,3,4));
 	return shuffled.slice(0, noOfFaulty);;
 	
@@ -253,6 +195,8 @@ function minigameSuccess(){
 }
 
 window.addEventListener('load',function(){
+	setUpInstructions();
 	setUp();
 	setUpSvg();
+	
 });
